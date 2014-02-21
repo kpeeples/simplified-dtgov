1019283108953c0b85f4722f6651831544264c75
Kenneth Peeples <kpeeples@redhat.com>
2014-02-21 02:07:41 -0500

    automated password file

--- install-run.sh
+++ install-run.sh
@@ -1,4 +1,5 @@
 echo "Running FSW Install Script"
-java -jar distros/jboss-fsw-installer-6.0.0.GA-redhat-4.jar support/InstallationScript.xml
+#java -jar <installer> /path/to/auto-install.xml -variablefile  /path/to/properties/file/containing/passwords 
+java -jar distros/jboss-fsw-installer-6.0.0.GA-redhat-4.jar support/InstallationScript.xml -variablefile support/InstallationScript.xml.variables
 echo "Start Server"
 installed/fsw/jboss-eap-6.1/bin/standalone.sh -c standalone.xml
